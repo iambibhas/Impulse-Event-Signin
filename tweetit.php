5f5c0368925edef6fa9cd281a6c22ec3e5e126c6
Bibhas <iambibhas@gmail.com>
2019-07-23 00:40:13 +0530

    Update tweetit.php

--- tweetit.php
+++ tweetit.php
@@ -2,11 +2,11 @@
 
 include 'twitteroauth/twitteroauth.php';
 
-$ckey = "UrZmndog89e0H3vaY7cg";
-$csecret = "q0D8fozwh60K1I1o1lx6QcvIjdljLq8sjhYVRfg";
+$ckey = "";
+$csecret = "";
 
-$oauthtoken = "279948462-7VCkJyFnRpIy0kOGbkqj3x2YHm6v9uaDaQwnQ4";
-$oauthtokensecret = "JCfhRdK3DG9PDbu78xtxSgJSjWLK8eoTfbzUn4";
+$oauthtoken = "";
+$oauthtokensecret = "";
 
 $message = urldecode($_REQUEST['msg']); //Command to be executed
 
